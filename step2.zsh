9cef14c2aa894b69d196b2649ddd49af2169773a
Lars Artmann <git@lars.software>
2022-08-27 08:24:07 +0200

    Init

---
+++ step2.zsh
@@ -0,0 +1,48 @@
+brew install git
+brew install node
+brew install gradle
+brew install maven
+brew install zip
+brew install openjdk@11
+brew install openjdk@17
+brew install kotlin
+brew install ki
+brew install ruby
+brew install go
+brew install terraform
+brew install terraformer
+brew install firebase-cli
+
+brew install wget
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+brew install --cask google-chrome
+brew install --cask chrome-remote-desktop-host
+brew install --cask spotify
+brew install --cask anydesk
+brew install --cask jetbrains-toolbox               
+brew install --cask intellij-idea
+brew install --cask betterdiscord-installer                               
+brew install --cask sublime-text                                       
+brew install --cask macpass
+brew install --cask google-drive
+brew install --cask google-cloud-sdk                                      
+brew install --cask signal
+brew install --cask openaudible                                      
+brew install --cask obs
+brew install --cask obs-virtualcam
+brew install --cask tor-browser
+brew install --cask multimc
+brew install --cask steam
+brew install --cask postman
+brew install --cask notion                                       
+brew install --cask android-commandlinetools
+brew install --cask microsoft-teams
+
+git config --global user.name "Lars Artmann"
+git config --global user.email "git@lars.software"
+ssh-keygen -o -t rsa -C "git@lars.software"
+
+gcloud init
+firebase login
+gcloud components install cbt alpha beta
