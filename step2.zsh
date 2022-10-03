579dd1767142af552f17f2c2c1c7e899ec9f3c20
Lars Artmann <lars.artmann.extern@hornbach.com>
2022-10-03 19:55:02 +0200

    Added raycast

--- step2.zsh
+++ step2.zsh
@@ -45,6 +45,7 @@ brew install --cask microsoft-teams
 brew install --cask iterm2
 brew install --cask steam
 brew install --cask whatsapp
+brew install --cask raycast
 
 git config --global user.name "Lars Artmann"
 git config --global user.email "git@lars.software"
