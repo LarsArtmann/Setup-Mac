90429124a789a2f549510c3cd1e80a1cd489a703
Lars Artmann <git@lars.software>
2024-06-27 01:39:35 +0200

    Added Brewfile dump update on add.sh

--- add.sh
+++ add.sh
@@ -2,6 +2,12 @@ command=$@
 $command &&
 echo "Added "$command" locally"
 echo "$command" >> step2.zsh &&
+echo "Backup old Brewfile..." &&
+mv Brewfile "$(shasum Brewfile | tr '  ' '_')" ||
+echo "Dump new Brewfile..." &&
+brew bundle dump &&
+echo "Git commit..." &&
 git commit -am "$command" &&
+echo "Git push..." &&
 git push &&
 echo "Added "$command" git"
