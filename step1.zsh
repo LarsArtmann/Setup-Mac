156e2e2996ca28b87ee6ff9b75a8943b27b39157
Lars Artmann <git@lars.software>
2025-02-03 01:33:41 +0100

    Added nix to step1

--- step1.zsh
+++ step1.zsh
@@ -1,3 +1,5 @@
+sh <(curl -L https://nixos.org/nix/install)
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
