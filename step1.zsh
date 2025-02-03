634c45e792988f033d8a93a8621e43ec289232b9
Lars Artmann <git@lars.software>
2025-02-03 02:29:07 +0100

    Added nix-darwin to step1.zsh

--- step1.zsh
+++ step1.zsh
@@ -1,4 +1,17 @@
+# Nix
 sh <(curl -L https://nixos.org/nix/install)
+
+# LnL7/nix-darwin
+sudo mkdir -p /etc/nix-darwin
+sudo chown $(id -nu):$(id -ng) /etc/nix-darwin
+cd /etc/nix-darwin
+
+# To use Nixpkgs:
+nix --extra-experimental-features "nix-command flakes" flake init -t nix-darwin/master
+
+sed -i '' "s/simple/$(scutil --get LocalHostName)/" flake.nix
+
+nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
